2df12fdf0790eaf35ff97401bf4480597ee89363
Andrew Sayman <andrew.sayman@gmail.com>
2025-02-05 15:32:58 -0500

    Get paths to all targets within limit

--- pathfinding.lua
+++ pathfinding.lua
@@ -163,3 +163,84 @@ function find_shortest_path_breadth(x, y, tx, ty, accept_child, no_path_func)
 	end
 	return reversed
 end
+
+-- @param x coordinate to start path finding at
+-- @param y coordinate to start path finding at
+-- @param limit stop once we've checked up to this distance
+-- @param accept_child  callback method (x,y), return true if you accept this child
+-- @param accept_target callback method (x,y), return true if you accept this as target
+-- @return a table of paths to accepted targets
+function find_targets_breadth(x, y, limit, accept_child, accept_target)
+
+	local visited = {}
+	local path = {}
+	local targets = {}
+
+	local queue = {}
+	local read = 0
+	local write = 0
+	write=write + 1  queue[write] = {x,y,0} -- push
+	visited[x..","..y] = true
+	path[x..","..y] = {to=nil, cost=0}
+	local dir={{x=-1,y=0},{x=1,y=0},{x=0,y=-1},{x=0,y=1} }
+
+	-- build table of path costs
+	while write > read do
+
+		-- pull currently visited node
+		read=read + 1  local coord = queue[read]  -- pull
+		local vx = coord[1]
+		local vy = coord[2]
+		local newcost = coord[3] + 1
+
+		-- position matching all criteria?
+		if accept_target(vx,vy) then
+			add(targets,{x=vx, y=vy })
+		end
+
+		-- go visit neighboars
+		for d in all(dir) do
+
+			-- child position
+			local cx = (vx+d.x)%48 -- map wrap
+			local cy = (vy+d.y)%48
+			if visited[cx..","..cy] == nil then
+				visited[cx..","..cy] = true
+				if accept_child(cx,cy) then
+					path[cx..","..cy] = {to={x=vx, y=vy}, cost=newcost}
+					if newcost <= limit then
+						write=write + 1  queue[write] = { cx, cy, newcost } -- push
+					end
+				end
+			end
+		end
+	end
+
+	for target in all(targets) do
+		local tx, ty = target.x, target.y
+	end
+
+	local targets_with_paths = {}
+	for target in all(targets) do
+		-- build reverse path
+		local tx, ty = target.x, target.y
+		local reversed = {{x=tx, y=ty}}
+		local pv = path[tx..","..ty]
+		if pv.to != nil then
+			while true do
+				if pv.to.x == x and pv.to.y == y then
+					break
+				end
+				add(reversed, pv.to)
+				pv = path[pv.to.x..","..pv.to.y]
+			end
+
+			-- return forward path
+			for i=1,#reversed\2 do
+				reversed[i],reversed[#reversed-i+1]=reversed[#reversed-i+1],reversed[i]
+			end
+			targets_with_paths[target] = reversed
+		end
+	end
+	return targets_with_paths
+end
